86eff2123288e7b880febfa63de6d4034e4bdd12
ABHIJITH-EA <abhijithae3773@gmail.com>
2024-08-21 15:07:38 +0530

    fix config module
    Add proper configuration values

--- frontend/src/config/apiConfig.js
+++ frontend/src/config/apiConfig.js
@@ -1,6 +1,6 @@
 
-const PORT = env.process.PORT || 8000
-const BASE_API_URL = `http://127.0.0.1:${8000}/api`;
+const PORT = 8000
+const BASE_API_URL = `http://127.0.0.1:${PORT}/api`;
 
 export const API_ENDPOINTS = {
     shortenUrl: `${BASE_API_URL}/short-url`,
